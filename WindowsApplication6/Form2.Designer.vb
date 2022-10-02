6b4c88f24fa18d8d558402c7a4e06e0b2ad253ef
Muhaimin <hazardemin@gmail.com>
2022-10-02 21:36:21 +0800

    hi

---
+++ WindowsApplication6/Form2.Designer.vb
@@ -0,0 +1,134 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form2
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.AccountToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ViewDetailsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.LogOutToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.InformationToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.CountryStatisticToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.DonorsInformationToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.VaccineInformationToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AboutToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.ImageScalingSize = New System.Drawing.Size(20, 20)
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AccountToolStripMenuItem, Me.InformationToolStripMenuItem, Me.AboutToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(577, 28)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'AccountToolStripMenuItem
+        '
+        Me.AccountToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ViewDetailsToolStripMenuItem, Me.LogOutToolStripMenuItem})
+        Me.AccountToolStripMenuItem.Name = "AccountToolStripMenuItem"
+        Me.AccountToolStripMenuItem.Size = New System.Drawing.Size(75, 24)
+        Me.AccountToolStripMenuItem.Text = "Account"
+        '
+        'ViewDetailsToolStripMenuItem
+        '
+        Me.ViewDetailsToolStripMenuItem.Name = "ViewDetailsToolStripMenuItem"
+        Me.ViewDetailsToolStripMenuItem.Size = New System.Drawing.Size(181, 26)
+        Me.ViewDetailsToolStripMenuItem.Text = "View Details"
+        '
+        'LogOutToolStripMenuItem
+        '
+        Me.LogOutToolStripMenuItem.Name = "LogOutToolStripMenuItem"
+        Me.LogOutToolStripMenuItem.Size = New System.Drawing.Size(181, 26)
+        Me.LogOutToolStripMenuItem.Text = "Log out"
+        '
+        'InformationToolStripMenuItem
+        '
+        Me.InformationToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.CountryStatisticToolStripMenuItem, Me.DonorsInformationToolStripMenuItem, Me.VaccineInformationToolStripMenuItem})
+        Me.InformationToolStripMenuItem.Name = "InformationToolStripMenuItem"
+        Me.InformationToolStripMenuItem.Size = New System.Drawing.Size(99, 24)
+        Me.InformationToolStripMenuItem.Text = "Information"
+        '
+        'CountryStatisticToolStripMenuItem
+        '
+        Me.CountryStatisticToolStripMenuItem.Name = "CountryStatisticToolStripMenuItem"
+        Me.CountryStatisticToolStripMenuItem.Size = New System.Drawing.Size(216, 26)
+        Me.CountryStatisticToolStripMenuItem.Text = "Country Statistic"
+        '
+        'DonorsInformationToolStripMenuItem
+        '
+        Me.DonorsInformationToolStripMenuItem.Name = "DonorsInformationToolStripMenuItem"
+        Me.DonorsInformationToolStripMenuItem.Size = New System.Drawing.Size(216, 26)
+        Me.DonorsInformationToolStripMenuItem.Text = "Donors Information"
+        '
+        'VaccineInformationToolStripMenuItem
+        '
+        Me.VaccineInformationToolStripMenuItem.Name = "VaccineInformationToolStripMenuItem"
+        Me.VaccineInformationToolStripMenuItem.Size = New System.Drawing.Size(216, 26)
+        Me.VaccineInformationToolStripMenuItem.Text = "Vaccine Information"
+        '
+        'AboutToolStripMenuItem
+        '
+        Me.AboutToolStripMenuItem.Name = "AboutToolStripMenuItem"
+        Me.AboutToolStripMenuItem.Size = New System.Drawing.Size(62, 24)
+        Me.AboutToolStripMenuItem.Text = "About"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(216, 349)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form2
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(577, 466)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form2"
+        Me.Text = "Form2"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents AccountToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ViewDetailsToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents LogOutToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents InformationToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents CountryStatisticToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents DonorsInformationToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents VaccineInformationToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents AboutToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents Button1 As Button
+End Class
